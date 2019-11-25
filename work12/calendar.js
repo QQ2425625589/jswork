286b5b47359b837258a9b8a4288516346a210a94
QQ2425625589 <2425625589@qq.com>
2019-11-25 14:47:09 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -3,9 +3,25 @@ function calendar(y){
     var html = '<div class="box">';
     for (var m=1;m<=12; ++m){
         html +='<table>'
-        html +='<tr class="title"><th colspan="7">' +y + '年' +m +'yue</th></tr>';
-        html +='<tr><td>ri</td><td>yi</td><td>er</td><td>san</td><td>si</td><td>wu</td><td>liu</td></tr>';
+        html +='<tr class="title"><th colspan="7">' +y + '年' +m +'月</th></tr>';
+        html +='<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
         var max = new Date(y,m,0).getDate();
-        html +=''
+        html +='<tr>';
+        for(var d=1; d<= max;++d){
+            if(w && d == 1){
+                html += '<td colspan="' + w + '"></td>';
+            }
+            html += '<td>' + d + '</td>';
+            if(w == 6 && d !==max){
+                html += '</tr><tr>';
+            }
+            else if (d == max){
+                html +='</tr>';
+            }
+            w = (w + 1 > 6) ? 0 : w + 1;
+        }
+        html += '</table>';
     }
+    html +='</div>';
+    return html;
 }
