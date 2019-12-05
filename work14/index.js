bea4c5079d1bac3b40d0e97b91ec0414442aa42c
QQ2425625589 <2425625589@qq.com>
2019-12-05 15:39:11 +0800

    work14

--- work14/index.js
+++ work14/index.js
@@ -7,20 +7,20 @@ function seckill() {
     if (remaining > 0) {
         d = parseInt(remaining / 86400);
         h = parseInt((remaining / 3600) % 24);
-        m = parseInt((remaining /60) % 60);
+        m = parseInt((remaining / 60) % 60);
         s = parseInt(remaining % 60);
         d = d < 10 ? '0' + d : d;
         h = h < 10 ? '0' + h : h;
         m = m < 10 ? '0' + m : m;
         s = s < 10 ? '0' + s : s;
 
-    }else {
+    } else {
         clearInterval(id);
         d = h = m = s = '00';
     }
     document.getElementById('d').innerHTML = d + '天';
-    document.getElementById('h').innerHTML = d + '时';
-    document.getElementById('m').innerHTML = d + '分';
-    document.getElementById('s').innerHTML = d + '秒';
+    document.getElementById('h').innerHTML = h + '时';
+    document.getElementById('m').innerHTML = m + '分';
+    document.getElementById('s').innerHTML = s + '秒';
 
 }
