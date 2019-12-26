72ab99775a89a169027c78d45fbffe3e9b9852df
QQ2425625589 <2425625589@qq.com>
2019-12-26 10:02:48 +0800

    ＂ｗｏｒｋ１９＂

---
+++ work19/index.js
@@ -0,0 +1,26 @@
+let ws
+function connect(){
+    let server = document.getElementById('server').nodeValue
+    ws = new WebSocket(server);
+    ws.onopen = function(){
+        document.getElementById('conn').disabled='disable';
+        document.getElementById('disconn').disabled='';
+        let(nickname){
+            ws.send('nickname |'+nickname)
+        }
+
+
+    }
+    ws.onclose = function(){
+        document.getElementById('conn').disabled='';
+        document.getElementById('disconn').disabled='disable';
+    }
+    ws.onmessage = function(event){
+        let board = document.getElementById('board')
+        let newmsg = document.createElement('div')
+        console.log(event.data)
+        newmsg.innerHTML= event.data
+        board.appendChild(newmsg)
+        board.scrollTop = board.scrollHeight;
+    }
+}
